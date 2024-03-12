5ea735c1ddad6e0daa1ed9db3d8159c3b2a926f8
Duncan McClean <duncan@duncanmcclean.com>
2024-03-12 20:27:28 +0000

    Add command to display rendering issue

--- routes/console.php
+++ routes/console.php
@@ -3,6 +3,18 @@
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
+use function Laravel\Prompts\confirm;
+
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+Artisan::command('long-confirm-label', function () {
+    $date = now()->format('Y-m-d');
+
+    if (confirm("The current date is [<comment>{$date}</comment>]. The label for this prompt is very long. Is this okay?")) {
+        $this->info('You chose to continue');
+    } else {
+        $this->info('You chose to cancel');
+    }
+});
